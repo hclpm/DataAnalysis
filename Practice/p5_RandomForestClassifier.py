12e5fca6178e0e0053ee2932751cd91077dd3e75
hclpm <144634536+hclpm@users.noreply.github.com>
2023-10-28 16:24:00 +0900

    Delete 폴더 directory

---
+++ Practice/p5_RandomForestClassifier.py
@@ -0,0 +1,24 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import RandomForestClassifier
+from sklearn.datasets import load_wine
+
+
+wineD = load_wine()
+x = wineD.data
+y = wineD.target
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=None)
+
+
+for i in range(5):
+   # 데이터 분할
+    # Random Forest 모델 학습
+    rf_classifier = DecisionTreeClassifier(random_state=None)
+    rf_classifier.fit(X_train, y_train)
+
+    # 테스트 데이터 예측
+    pred = rf_classifier.predict(X_test)
+
+    # 정확도 계산
+    accuracy = accuracy_score(y_test, pred)
+    print(f"Act{i:2} Accuracy:", accuracy)
